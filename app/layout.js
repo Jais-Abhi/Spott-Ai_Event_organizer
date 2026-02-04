1a98ebaca4021c4a42c2d2bfc6ad08ac9f4d9f03
Abhishek jaiswal <abhijaiswal7479@gmail.com>
2026-02-04 17:51:29 +0530

    layout : m and p fixed

--- app/layout.js
+++ app/layout.js
@@ -56,7 +56,6 @@ export default function RootLayout({ children }) {
               </div>
 
               <div
-              className=""
               >
                 {children}
               </div>
--- components/Header.jsx
+++ components/Header.jsx
@@ -13,7 +13,8 @@ const Header = () => {
     const {isLoading, isAuthenticated} = useStoreUser();
     console.log(isLoading);
   return (
-    <nav className=' bg-linear-to-r  fixed top-0 from-black via-gray-950/60 to-gray-950/40 w-full z-10 ' >
+    <nav className=' bg-linear-to-r text-4xl  fixed top-0 from-black via-gray-950/60 to-gray-950/40 w-full z-10 ' >
+        
         <div className='flex h-20 justify-around items-center ' >
             <Link href="/">
                 <Image src="/spott.png" width={90} height={40} alt="Logo" />
@@ -21,9 +22,23 @@ const Header = () => {
             <div className='w-1/3' >
                 search
             </div>
-            <div>
+            <div className='flex justify-center items-center gap-14 mr-20' >
+                <div className=' pr-12 '>
+                    hello
+                    <Button variant='ghost' asChild  >
+                        <Link href={"/pricing"} className='mr-12' >
+                        Pricing </Link>
+                    </Button>
+                </div>
+                <div className='p-8' >
+                    button
+                </div>
+                <div className='p-8' >
+                    button
+                </div>
                 {/* Show the user button when the user is signed in */}
-                <Authenticated>
+                <div className='pr-[10rem]' >
+                    <Authenticated>
                     <UserButton />
                 </Authenticated>
 
@@ -33,6 +48,7 @@ const Header = () => {
                         <Button size="sm" >sign In</Button>
                     </SignInButton>
                 </Unauthenticated>
+                </div>
             
             </div>
         </div>
